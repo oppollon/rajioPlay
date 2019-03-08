429527e5c78bf2763057cc11f1eb4157d5c5a17c
Kuro <tatomete@protonmail.com>
2019-03-08 14:05:50 +0100

    Projektdateien hinzufügen.

---
+++ rajioPlay/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace rajioPlay
+{
+    partial class rajio
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(rajio));
+            this.ntfPlayer = new System.Windows.Forms.NotifyIcon(this.components);
+            this.rajioMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.radioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.radioToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.edenOfTheWestToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listenmoeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.currentSongToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.showToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.copyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.playToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rajioMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ntfPlayer
+            // 
+            this.ntfPlayer.ContextMenuStrip = this.rajioMenu;
+            this.ntfPlayer.Icon = ((System.Drawing.Icon)(resources.GetObject("ntfPlayer.Icon")));
+            this.ntfPlayer.Text = "uwu I\'m currently watching you, sicko pedo";
+            this.ntfPlayer.Visible = true;
+            // 
+            // rajioMenu
+            // 
+            this.rajioMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.radioToolStripMenuItem,
+            this.currentSongToolStripMenuItem,
+            this.playToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.rajioMenu.Name = "rajioMenu";
+            this.rajioMenu.Size = new System.Drawing.Size(181, 114);
+            // 
+            // radioToolStripMenuItem
+            // 
+            this.radioToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.radioToolStripMenuItem1,
+            this.edenOfTheWestToolStripMenuItem,
+            this.listenmoeToolStripMenuItem});
+            this.radioToolStripMenuItem.Name = "radioToolStripMenuItem";
+            this.radioToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.radioToolStripMenuItem.Text = "Radio";
+            // 
+            // radioToolStripMenuItem1
+            // 
+            this.radioToolStripMenuItem1.Name = "radioToolStripMenuItem1";
+            this.radioToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.radioToolStripMenuItem1.Text = "r/a/dio";
+            this.radioToolStripMenuItem1.Click += new System.EventHandler(this.radioToolStripMenuItem1_Click);
+            // 
+            // edenOfTheWestToolStripMenuItem
+            // 
+            this.edenOfTheWestToolStripMenuItem.Name = "edenOfTheWestToolStripMenuItem";
+            this.edenOfTheWestToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.edenOfTheWestToolStripMenuItem.Text = "eden";
+            this.edenOfTheWestToolStripMenuItem.Click += new System.EventHandler(this.edenOfTheWestToolStripMenuItem_Click);
+            // 
+            // listenmoeToolStripMenuItem
+            // 
+            this.listenmoeToolStripMenuItem.Name = "listenmoeToolStripMenuItem";
+            this.listenmoeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.listenmoeToolStripMenuItem.Text = "listen.moe";
+            this.listenmoeToolStripMenuItem.Click += new System.EventHandler(this.listenmoeToolStripMenuItem_Click);
+            // 
+            // currentSongToolStripMenuItem
+            // 
+            this.currentSongToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showToolStripMenuItem,
+            this.copyToolStripMenuItem});
+            this.currentSongToolStripMenuItem.Name = "currentSongToolStripMenuItem";
+            this.currentSongToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.currentSongToolStripMenuItem.Text = "Current song";
+            // 
+            // showToolStripMenuItem
+            // 
+            this.showToolStripMenuItem.Name = "showToolStripMenuItem";
+            this.showToolStripMenuItem.Size = new System.Drawing.Size(105, 22);
+            this.showToolStripMenuItem.Text = "Show";
+            this.showToolStripMenuItem.Click += new System.EventHandler(this.showToolStripMenuItem_Click);
+            // 
+            // copyToolStripMenuItem
+            // 
+            this.copyToolStripMenuItem.Name = "copyToolStripMenuItem";
+            this.copyToolStripMenuItem.Size = new System.Drawing.Size(105, 22);
+            this.copyToolStripMenuItem.Text = "Copy";
+            this.copyToolStripMenuItem.Click += new System.EventHandler(this.copyToolStripMenuItem_Click);
+            // 
+            // playToolStripMenuItem
+            // 
+            this.playToolStripMenuItem.Name = "playToolStripMenuItem";
+            this.playToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.playToolStripMenuItem.Text = "Stop";
+            this.playToolStripMenuItem.Click += new System.EventHandler(this.playToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // rajio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(191, 96);
+            this.Name = "rajio";
+            this.Text = "rajio";
+            this.rajioMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon ntfPlayer;
+        private System.Windows.Forms.ContextMenuStrip rajioMenu;
+        private System.Windows.Forms.ToolStripMenuItem radioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem radioToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem edenOfTheWestToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem listenmoeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem currentSongToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem showToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem copyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem playToolStripMenuItem;
+    }
+}
+
---
+++ rajioPlay/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Un4seen.Bass;
+
+namespace rajioPlay
+{
+    public partial class rajio : Form
+    {
+        public int channel = 0;
+        public rajio()
+        {
+            InitializeComponent();
+
+            //Hide app
+            this.WindowState = FormWindowState.Minimized;
+            this.FormBorderStyle = FormBorderStyle.SizableToolWindow;
+            this.ShowInTaskbar = false;
+
+            //Load
+            onLoad();
+        }
+
+        public void onLoad()
+        {
+            if(this.WindowState == FormWindowState.Minimized)
+            {
+                try
+                {
+                    Bass.BASS_SetConfig(BASSConfig.BASS_CONFIG_NET_PLAYLIST, 2);
+                    Bass.BASS_StreamFree(channel);
+                    Bass.BASS_Init(-1, 44100, BASSInit.BASS_DEVICE_DEFAULT, IntPtr.Zero); //Init BASS
+                }
+                catch
+                {
+                    ntfPlayer.ShowBalloonTip(1000, "Dameee!", "Rajio's got just hurt! :(", ToolTipIcon.Error);
+                }
+                ntfPlayer.ShowBalloonTip(1000, "Ohayoo!", "Rajio's is waiting in the notification tray!", ToolTipIcon.None);
+
+            }
+        }
+        public string CurrentPlay()
+        {
+            string[] b = Bass.BASS_ChannelGetTagsMETA(channel);
+            if (b == null) return "Unknown :(";
+            int sep = b[0].IndexOf(";");
+            if (sep == 0) return "Unknown :(";
+            string title = b[0].Substring(0, sep);
+            title = title.Substring(title.IndexOf("=") + 1).Replace("'", "");
+            if (title == "") return "Unknown :(";
+            return title;
+        }
+        public void playRadioSong(string url)
+        {
+            try
+            {
+                if (Bass.BASS_ChannelPlay(channel, false))
+                {
+                    Bass.BASS_ChannelStop(channel);
+                }
+                channel = Bass.BASS_StreamCreateURL(url, 0, BASSFlag.BASS_DEFAULT, null, IntPtr.Zero);
+            }
+            catch
+            {
+                ntfPlayer.ShowBalloonTip(1500, "Eeeeeeeh?", "BASS Error:" + Bass.BASS_ErrorGetCode(), ToolTipIcon.Error);
+            }
+            Bass.BASS_ChannelPlay(channel, false);
+
+        }
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void radioToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            playRadioSong("https://relay0.r-a-d.io/main.mp3");
+        }
+
+        private void edenOfTheWestToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            playRadioSong("http://edenofthewest.com:8080/eden.mp3");
+        }
+
+        private void listenmoeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            playRadioSong("https://listen.moe/fallback");
+        }
+
+        private void showToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ntfPlayer.ShowBalloonTip(1000, "Current song", CurrentPlay(), ToolTipIcon.None);
+        }
+
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Clipboard.SetText(CurrentPlay());
+        }
+
+        private void playToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (Bass.BASS_Start())
+            {
+                Bass.BASS_ChannelStop(channel);
+            }
+        }
+    }
+}
